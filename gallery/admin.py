e5d9fd77e6511852b91e3076dc1ce48ab64f05f6
moharick <moharick@gmail.com>
2019-10-07 16:33:09 +0300

    This commit updates the migrations

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Image,Location,categories
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal=("categories",)
+
+
+admin.site.register(Image,ImageAdmin)
+admin.site.register(categories)
+admin.site.register(Location)
